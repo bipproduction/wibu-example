fdcf2563a9bab2507d316e55f9add95332b51752
bipproduction <bip.production.js@gmail.com>
2024-09-18 17:50:42 +0800

    fix: perbarui sourcemap pada service worker untuk mencerminkan perubahan di sw.js
    Pesan commit di atas menjelaskan perbuatan spesifik yang dilakukan pada file sourcemap dari service worker, yang mencakup pembaruan untuk mencerminkan perubahan pada file . Pesan ini menggunakan kata kerja imperatif perbarui dan menyediakan konteks yang cukup untuk mengidentifikasi jenis perubahan tanpa memerlukan detail yang tidak perlu. Selain itu, menggunakan tipe commit fix mengindikasikan bahwa ini adalah perbaikan atau pembaruan yang diperlukan karena perubahan sebelumnya.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
 // Import styles of packages that you've installed.
 // All packages except `@mantine/hooks` require styles imports
-import '@mantine/core/styles.css';
-import './globals.css';
+import "@mantine/core/styles.css";
+import "./globals.css";
 
-import { ColorSchemeScript, MantineProvider } from '@mantine/core';
+import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 
 import type { Metadata, Viewport } from "next";
+import { ScrollProvider } from "@/lib/ui/ScrollProvider";
 
 const APP_NAME = "WIBU EXAMPLE";
 const APP_DEFAULT_TITLE = "wibu example";
@@ -16,55 +17,56 @@ export const metadata: Metadata = {
   applicationName: APP_NAME,
   title: {
     default: APP_DEFAULT_TITLE,
-    template: APP_TITLE_TEMPLATE,
+    template: APP_TITLE_TEMPLATE
   },
   description: APP_DESCRIPTION,
   manifest: "/manifest.json",
   appleWebApp: {
     capable: true,
     statusBarStyle: "default",
-    title: APP_DEFAULT_TITLE,
+    title: APP_DEFAULT_TITLE
     // startUpImage: [],
   },
   formatDetection: {
-    telephone: false,
+    telephone: false
   },
   openGraph: {
     type: "website",
     siteName: APP_NAME,
     title: {
       default: APP_DEFAULT_TITLE,
-      template: APP_TITLE_TEMPLATE,
+      template: APP_TITLE_TEMPLATE
     },
-    description: APP_DESCRIPTION,
+    description: APP_DESCRIPTION
   },
   twitter: {
     card: "summary",
     title: {
       default: APP_DEFAULT_TITLE,
-      template: APP_TITLE_TEMPLATE,
+      template: APP_TITLE_TEMPLATE
     },
-    description: APP_DESCRIPTION,
-  },
+    description: APP_DESCRIPTION
+  }
 };
 
 export const viewport: Viewport = {
-  themeColor: "#FFFFFF",
+  themeColor: "#FFFFFF"
 };
 
 export default function RootLayout({
-  children,
+  children
 }: {
   children: React.ReactNode;
 }) {
-
   return (
     <html lang="en">
       <head>
-        <ColorSchemeScript defaultColorScheme='dark' />
+        <ColorSchemeScript defaultColorScheme="dark" />
       </head>
       <body>
-        <MantineProvider defaultColorScheme='dark' >{children}</MantineProvider>
+        <MantineProvider defaultColorScheme="dark">
+          <ScrollProvider>{children}</ScrollProvider>
+        </MantineProvider>
       </body>
     </html>
   );
